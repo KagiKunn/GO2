b3239af66414b4580b985649eedc314e70fee0d4
KagiKun <key9910@gmail.com>
2024-07-22 16:19:57 +0900

    Feat : Change defense direction

--- Assets/Scripts/DefenseScripts/EnemyMovement.cs
+++ Assets/Scripts/DefenseScripts/EnemyMovement.cs
@@ -6,17 +6,43 @@ public class EnemyMovement : MonoBehaviour
 {
     [SerializeField]
     private float speed = 1.0f;
+    [SerializeField]
+    private LayerMask detectionLayerMask;
+    
+    [SerializeField]
+    private Vector2 boxSize = new Vector2(2, 1);
     private Rigidbody2D _rigid2d;
     private Animator _animator;
+    private static readonly int RunAnimationHash = Animator.StringToHash("1_Run");
+    private static readonly int AttackAnimationHash = Animator.StringToHash("2_Attack_Bow");
+    private Vector3 movementdirection;
     private void Awake()
     {
+        // pos.position = new Vector2(0, 0);
         _rigid2d = GetComponent<Rigidbody2D>();
         _animator = GetComponent<Animator>();
+        movementdirection = Vector3.left;
     }
 
     private void Update()
     {
-        _rigid2d.velocity = Vector3.down * (speed * Time.timeScale);
-        _animator.Play("1_Run");
+        Vector2 boxCenter = (Vector2)transform.position + new Vector2(-boxSize.x/2, 0);
+        Collider2D hit = Physics2D.OverlapBox(boxCenter, boxSize, 0,detectionLayerMask);
+        if (hit != null && hit.name == "CastleWall")
+            {
+                _animator.Play(AttackAnimationHash);
+                movementdirection = Vector3.zero;
+            }
+        else
+        {
+            _animator.Play(RunAnimationHash);
+        }
+            _rigid2d.velocity = movementdirection * (speed * Time.timeScale);
+    }
+    private void OnDrawGizmos()
+    {
+        Vector2 boxCenter = (Vector2)transform.position + new Vector2(-boxSize.x/2, 0);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(boxCenter, boxSize);
     }
 }
--- Assets/Scripts/DefenseScripts/EnemySpawner.cs
+++ Assets/Scripts/DefenseScripts/EnemySpawner.cs
@@ -1,17 +1,20 @@
+using System;
 using UnityEngine;
 using System.Collections;
+using Unity.Mathematics;
+using Random = UnityEngine.Random;
 
 public class EnemySpawner : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public GameObject objectPrefab;
-    
+
     // 오브젝트를 생성할 X축의 최소 및 최대 좌표
-    public float minX = -10f;
-    public float maxX = 10f;
+    public float minY = -0f;
+    public float maxY = 10f;
 
     // 고정된 Y축 좌표
-    private const float fixedY = 10.5f;
+    private const float fixedX = 9.5f;
 
     // 생성할 오브젝트의 개수
     [SerializeField]
@@ -28,11 +31,11 @@ public class EnemySpawner : MonoBehaviour
     {
         for (int i = 0; i < numberOfObjects; i++)
         {
-            // X축의 랜덤 좌표 생성
-            float randomX = Random.Range(minX, maxX);
+            // Y축의 랜덤 좌표 생성
+            float randomY = Random.Range(minY, maxY);
 
             // 새로운 오브젝트 생성
-            Vector3 spawnPosition = new Vector3(randomX, fixedY, 0);
+            Vector3 spawnPosition = new Vector3(fixedX, randomY, 0);
             GameObject enemy = Instantiate(objectPrefab, spawnPosition, Quaternion.identity, transform);
 
             float waitTime = Random.Range(0, maxSpawnInterval);
