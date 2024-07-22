fd45380762336cfc81264f78a2cf4b05c21fbe64
PJW <PJW>
2024-07-22 17:29:35 +0900

    add GAMEOVER image

--- Assets/Scripts/DefenseScripts/CastleWall.cs
+++ Assets/Scripts/DefenseScripts/CastleWall.cs
@@ -1,10 +1,23 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CastleWall : MonoBehaviour
 {
    [SerializeField]
    public int health = 3000; // 성벽의 초기 체력
+
+   [SerializeField] private Image gameOverImage; // 겜오버 이미지 참조변수
+
+
+   private void Start()
+   {
+      if (gameOverImage != null)
+      {
+         gameOverImage.gameObject.SetActive(false);
+      }
+   }
+
 
    // 성벽이 공격당했을 때 체력을 감소시키는 함수
    public void TakeDamage(int damage)
@@ -15,6 +28,15 @@ public class CastleWall : MonoBehaviour
       {
          Destroy(gameObject);
          Debug.Log("성벽이 파괴되었습니다!");
+         ShowGameOverImage();
+      }
+   }
+
+   private void ShowGameOverImage()
+   {
+      if (gameOverImage != null)
+      {
+         gameOverImage.gameObject.SetActive(true);
       }
    }
 }
