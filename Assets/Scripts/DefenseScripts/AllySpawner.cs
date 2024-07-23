a982de2fcdcf475f4cdd2cdc7db29c7c4bdecd7d
KagiKun <key9910@gmail.com>
2024-07-23 18:51:14 +0900

    allies spawn fin

--- Assets/Scripts/DefenseScripts/AllySpawner.cs
+++ Assets/Scripts/DefenseScripts/AllySpawner.cs
@@ -7,6 +7,8 @@ public class AllySpawner : MonoBehaviour
     [SerializeField] private GameObject[] allies;
     [SerializeField] private Tilemap tilemap;
     [SerializeField] private bool facingRight;
+    [SerializeField] private float xSpacing = 0.5f;
+    [SerializeField] private float ySpacing = 0.5f;
     private Vector3[] alliesPosition;
 
     private int row = 8;
@@ -17,7 +19,7 @@ public class AllySpawner : MonoBehaviour
         int maxUnitCount = allies.Length-1;
         int currentNumber = 0;
         BoundsInt bounds = tilemap.cellBounds;
-        Vector3Int gridCenter = new Vector3Int(bounds.xMin + bounds.size.x / 2, bounds.yMin + bounds.size.y / 2, 0);
+        Vector3Int gridCenter = new Vector3Int(bounds.xMin + bounds.size.x / 3, bounds.yMin + bounds.size.y / 2, 0);
         for (int x = 0; x < column; x++)
         {
             for (int y = 0; y < row; y++)
@@ -28,6 +30,7 @@ public class AllySpawner : MonoBehaviour
                 }
                 Vector3Int gridPosition = new Vector3Int(gridCenter.x + x+1, y, 0);
                 Vector3 worldPosition = tilemap.CellToWorld(gridPosition);
+                worldPosition += new Vector3(x * xSpacing, y * ySpacing, 0);
                 if (facingRight) {
                     Instantiate(allies[currentNumber], worldPosition, Quaternion.Euler(0,180,0),transform);
 
