4057bd4b40c584f738e3995e22f07fc8cf4a858e
KagiKun <key9910@gmail.com>
2024-07-30 09:58:50 +0900

    Fix : defense

---
+++ Assets/KMS Folder/Script/BtnScene.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class BtnScene : MonoBehaviour
+{
+   public void heroSceneChange()
+   {
+       SceneManager.LoadScene("HeroManagement");
+       // 편성한 영웅 정보 불러오기
+       
+       CustomLogger.Log("Change HeroManagementScene successfuly!");
+   }
+
+   public void internalSceneChange()
+   {
+       SceneManager.LoadScene("InternalAffairs");
+       CustomLogger.Log("Change InternalAffairsScene successfuly!");
+   }
+}
