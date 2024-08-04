b3c891fdfd105cedb36d2fe7e17e4fb7b6d26785
PJW <haren3415@gmail.com>
2024-08-04 19:03:12 +0900

    fixing boss death trigger

---
+++ Assets/Scripts/DefenseScripts/BossBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefenseScripts
+{
+    public class BossBehaviour : MonoBehaviour
+    {
+        public event Action OnBossDisabledEvent;
+
+        private void OnDisable()
+        {
+            CustomLogger.Log("BossBehaviour OnDisable 호출됨", "yellow");
+            OnBossDisabledEvent?.Invoke();
+        }
+    }
+}
