0704159ef659cc435bfd292bdb9c91a9516cfef8
PJW <PJW>
2024-08-05 13:14:09 +0900

    Refactoring Spawn Logic

---
+++ Assets/Scripts/DefenseScripts/HumanPrefabs.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class HumanPrefabs : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> humanPrefabs;
+    [SerializeField] private GameObject humanBossPrefab;
+
+    public List<GameObject> GetPrefabs()
+    {
+        return humanPrefabs;
+    }
+    
+    public GameObject GetBossPrefab()
+    {
+        return humanBossPrefab;
+    }
+}
