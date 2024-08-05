67d6fef3ff51480cde1d0978f0933536f88ced6f
11lucy22 <winca1122@naver.com>
2024-08-05 17:14:10 +0900

    Fix HeroGameManager.cs, HeroSelect.cs

--- Assets/KMS Folder/Script/HeroGameManager.cs
+++ Assets/KMS Folder/Script/HeroGameManager.cs
@@ -18,55 +18,45 @@ public class HeroGameManager : MonoBehaviour
     void Awake()
     {
         filePath = Path.Combine(Application.persistentDataPath, "selectedHeroes.json");
-        LoadHeroData();
-        LoadHeroFormation();
-    }
-
-    public void LoadHeroData()
-    {
-        heroDataList = new List<HeroData>(Resources.LoadAll<HeroData>("ScriptableObjects/Heroes"));
-        if (heroDataList == null || heroDataList.Count == 0)
-        {
-            CustomLogger.Log("No HeroData found in Resources/ScriptableObjects/Heroes");
-        }
-        
     }
     public void SaveHeroFormation()
     {
         HeroDataWrapper wrapper = new HeroDataWrapper { Heroes = selectedHeroes };
-
         string json = JsonUtility.ToJson(wrapper, true);
         File.WriteAllText(filePath, json);
     }
 
     public void LoadHeroFormation()
     {
-        if (!File.Exists(filePath))
+        if (File.Exists(filePath))
         {
-            CustomLogger.Log("No hero formation file found.");
-            return;
-        }
-        try
-        {
-            string json = File.ReadAllText(filePath);
-            HeroDataWrapper wrapper = JsonUtility.FromJson<HeroDataWrapper>(json);
-            
-            selectedHeroes.Clear();
-            
-            foreach (HeroData hero in wrapper.Heroes)
+            try
             {
-                if ( selectedHeroes.Count < 3 && !selectedHeroes.Exists(h => h.Name == hero.Name))
+                string json = File.ReadAllText(filePath);
+                HeroDataWrapper wrapper = JsonUtility.FromJson<HeroDataWrapper>(json);
+
+                selectedHeroes.Clear();
+
+                foreach (HeroData hero in wrapper.Heroes)
                 {
-                    selectedHeroes.Add(hero);
+                    if (selectedHeroes.Count < 3 && !selectedHeroes.Exists(h => h.Name == hero.Name))
+                    {
+                        selectedHeroes.Add(hero);
+                    }
                 }
             }
-        }
-        catch (Exception e)
-        {
-            CustomLogger.Log($"Error loading hero formation: {e.Message}", "red");
+            catch (Exception e)
+            {
+                CustomLogger.Log($"Error loading hero formation: {e.Message}", "red");
+            }
         }
     }
-    
+
+    public void ClearHeroFormation()
+    {
+        selectedHeroes.Clear();
+        SaveHeroFormation();
+    }
     public void AddSelectedHero(HeroData hero)
     {
         if (selectedHeroes.Count < 3 && !selectedHeroes.Exists(h => h.Name == hero.Name))
