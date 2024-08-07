b15852a3810c1829bc2772a72f22294581348900
hideyoshi515 <hideyoshi515@gmail.com>
2024-08-07 17:10:44 +0900

    UnitUpgrade

--- Assets/Scripts/UnitUpgrade.cs
+++ Assets/Scripts/UnitUpgrade.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 
@@ -25,8 +26,51 @@ public class UnitUpgrade : MonoBehaviour
     [ArrayElementTitle("className")] [SerializeField]
     public UnitList[] upgradeList;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public GameObject leftBox, rightBox, origin, upgrade;
+    public int[] levelList = new int[4];
+    public int classNum = 0;
+
     void Start()
+    {
+    }
+
+    void Awake()
+    {
+        leftBox = GameObject.Find("UnitFormation");
+        rightBox = GameObject.Find("UnitFormation(Reinforce)");
+        origin = Instantiate(upgradeList[classNum].UnitListArray[levelList[0]]);
+        upgrade = Instantiate(upgradeList[classNum].UnitListArray[levelList[0] + 1]);
+        origin.transform.localScale = new Vector3(2.5f, 2.5f);
+        upgrade.transform.localScale = new Vector3(2.5f, 2.5f);
+        origin.transform.position = leftBox.transform.position;
+        upgrade.transform.position = rightBox.transform.position;
+    }
+
+    public void Upgrade()
+    {
+        if (classNum == 3)
+        {
+        }
+        else
+        {
+            if (levelList[classNum] >= upgradeList[classNum].UnitListArray.Length - 1) return;
+            Destroy(origin);
+            Destroy(upgrade);
+            levelList[classNum] += 1;
+            origin = Instantiate(upgradeList[classNum].UnitListArray[levelList[classNum]]);
+            origin.transform.localScale = new Vector3(2.5f, 2.5f);
+            origin.transform.position = leftBox.transform.position;
+            if (levelList[classNum] < upgradeList[classNum].UnitListArray.Length - 1)
+            {
+                upgrade = Instantiate(upgradeList[classNum].UnitListArray[levelList[0] + 1]);
+                upgrade.transform.localScale = new Vector3(2.5f, 2.5f);
+                upgrade.transform.position = rightBox.transform.position;
+            }
+        }
+    }
+
+    void debugOutput()
     {
         for (int i = 0; i < upgradeList.Length; i++)
         {
@@ -59,10 +103,5 @@ public class UnitUpgrade : MonoBehaviour
                 }
             }
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
     }
 }
