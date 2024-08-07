a297b22dfb43edbe1d0c4bb8261005fd56ae4abb
hideyoshi515 <hideyoshi515@gmail.com>
2024-08-07 09:48:03 +0900

    Fix Inventory Bugs

--- Assets/Scripts/InventoryUI.cs
+++ Assets/Scripts/InventoryUI.cs
@@ -1,135 +1,162 @@
 using UnityEngine;
 using UnityEngine.UI;
-
 using TMPro;
-
 using UnityEditor;
 
-public class InventoryUI : MonoBehaviour {
-	private Canvas canvas;
-	private ScrollRect scrollRect;
-	public Transform inventoryContent;
-	private const string inventoryFilePath = "inventory.json";
 
-	public InventoryData inventoryData = new InventoryData();
+public class InventoryUI : MonoBehaviour
+{
+    private Canvas canvas;
+    private ScrollRect scrollRect;
+    public Transform inventoryContent;
+    private const string inventoryFilePath = "inventory.json";
 
-	private void Start() {
-		canvas = GetComponent<Canvas>();
-		scrollRect = GetComponentInChildren<ScrollRect>();
-		LoadInventory();
-	}
+    public InventoryData inventoryData = new InventoryData();
 
-	public void CanvasEnabled() {
-		canvas.enabled = !canvas.enabled;
+    private void Start()
+    {
+        canvas = GetComponent<Canvas>();
+        scrollRect = GetComponentInChildren<ScrollRect>();
+        LoadInventory();
+    }
 
-		if (canvas.enabled) {
-			ResetScrollRect();
-			UpdateInventoryUI();
-		}
-	}
+    public void CanvasEnabled()
+    {
+        canvas.enabled = !canvas.enabled;
+        if (canvas.enabled)
+        {
+            ResetScrollRect();
+            UpdateInventoryUI();
+        }
+    }
 
-	private void ResetScrollRect() {
-		scrollRect.verticalNormalizedPosition = 1f;
-	}
+    private void ResetScrollRect()
+    {
+        scrollRect.verticalNormalizedPosition = 1f;
+    }
 
-	public void AddItemToInventory(ItemSO item) {
-		if (CanAddItem()) {
-			inventoryData.items.Add(item);
-			SaveInventory();
-			UpdateInventoryUI();
-		} else {
-			CustomLogger.LogWarning("인벤토리에 더 이상 아이템을 추가할 수 없습니다.");
-		}
-	}
+    public void AddItemToInventory(ItemSO item)
+    {
+        CanAddItem();
+        inventoryData.items.Add(item);
+        SaveInventory();
+        UpdateInventoryUI();
+    }
 
-	public bool CanAddItem() {
-		return inventoryData.items.Count < inventoryContent.childCount;
-	}
+    public bool CanAddItem()
+    {
+        return inventoryData.items.Count < inventoryContent.childCount;
+    }
 
-	public void UpdateInventoryUI() {
-		for (int i = 0; i < inventoryData.items.Count; i++) {
-			Transform slot = inventoryContent.GetChild(i);
-			UpdateSlotUI(slot, inventoryData.items[i]);
-		}
+    public bool CanAdditems(int itemCount)
+    {
+        return inventoryData.items.Count + itemCount <= inventoryContent.childCount;
+    }
 
-		for (int i = inventoryData.items.Count; i < inventoryContent.childCount; i++) {
-			Transform slot = inventoryContent.GetChild(i);
-			UpdateSlotUI(slot, null);
-		}
-	}
+    public void UpdateInventoryUI()
+    {
+        for (int i = 0; i < inventoryData.items.Count; i++)
+        {
+            Transform slot = inventoryContent.GetChild(i);
+            UpdateSlotUI(slot, inventoryData.items[i]);
+        }
 
-	private void UpdateSlotUI(Transform slot, ItemSO item) {
-		Image itemImage = slot.Find("ItemImage").GetComponent<Image>();
-		TextMeshProUGUI itemName = slot.Find("ItemName").GetComponent<TextMeshProUGUI>();
+        for (int i = inventoryData.items.Count; i < inventoryContent.childCount; i++)
+        {
+            Transform slot = inventoryContent.GetChild(i);
+            UpdateSlotUI(slot, null);
+        }
+    }
 
-		if (itemImage == null) {
-			CustomLogger.LogError("ItemImage component is missing on ItemImage GameObject.");
-		}
+    private void UpdateSlotUI(Transform slot, ItemSO item)
+    {
+        Image itemImage = slot.Find("ItemImage").GetComponent<Image>();
+        TextMeshProUGUI itemName = slot.Find("ItemName").GetComponent<TextMeshProUGUI>();
 
-		if (itemName == null) {
-			CustomLogger.LogError("Text component is missing on ItemName GameObject.");
-		}
+        if (itemImage == null)
+        {
+            CustomLogger.LogError("ItemImage component is missing on ItemImage GameObject.");
+        }
 
-		if (item != null) {
-			itemImage.sprite = item.icon;
-			itemName.text = item.itemName;
-		} else {
-			itemImage.sprite = null;
-			itemName.text = "";
-		}
-	}
+        if (itemName == null)
+        {
+            CustomLogger.LogError("Text component is missing on ItemName GameObject.");
+        }
 
-	void SaveInventory() {
-		JsonInventory.SaveToJson(inventoryData, inventoryFilePath);
-	}
+        if (item != null)
+        {
+            itemImage.sprite = item.icon;
+            itemName.text = item.itemName;
+        }
+        else
+        {
+            itemImage.sprite = null;
+            itemName.text = "";
+        }
+    }
 
-	void LoadInventory() {
-		inventoryData = JsonInventory.LoadFromJson<InventoryData>(inventoryFilePath) ?? new InventoryData();
+    void SaveInventory()
+    {
+        JsonInventory.SaveToJson(inventoryData, inventoryFilePath);
+    }
 
-		int currentSlotCount = inventoryContent.childCount;
+    void LoadInventory()
+    {
+        inventoryData = JsonInventory.LoadFromJson<InventoryData>(inventoryFilePath) ?? new InventoryData();
 
-		for (int i = 0; i < inventoryData.additionalSlotCount; i++) {
-			GameObject newSlot =
-				(GameObject)AssetDatabase.LoadAssetAtPath("Assets/JSFolder/PreFab/Slot.prefab", typeof(GameObject));
+        int currentSlotCount = inventoryContent.childCount;
 
-			if (newSlot != null) {
-				Instantiate(newSlot, inventoryContent);
-			}
-		}
+        for (int i = 0; i < inventoryData.additionalSlotCount; i++)
+        {
+            GameObject newSlot =
+                Resources.Load<GameObject>("Assets/JSFolder/PreFab/Slot.prefab");
+            if (newSlot != null)
+            {
+                Instantiate(newSlot, inventoryContent);
+            }
+        }
 
-		UpdateInventoryUI();
-	}
+        UpdateInventoryUI();
+    }
 
-	public void ClearInventory() {
-		inventoryData.items.Clear();
+    public void ClearInventory()
+    {
+        inventoryData.items.Clear();
 
-		int baseSlotCount = inventoryContent.childCount - inventoryData.additionalSlotCount;
+        int baseSlotCount = inventoryContent.childCount - inventoryData.additionalSlotCount;
+        for (int i = inventoryContent.childCount - 1; i >= baseSlotCount; i--)
+        {
+            Destroy(inventoryContent.GetChild(i).gameObject);
+        }
 
-		for (int i = inventoryContent.childCount - 1; i >= baseSlotCount; i--) {
-			Destroy(inventoryContent.GetChild(i).gameObject);
-		}
+        inventoryData.additionalSlotCount = 0;
+        SaveInventory();
+        UpdateInventoryUI();
+    }
 
-		inventoryData.additionalSlotCount = 0;
-		SaveInventory();
-		UpdateInventoryUI();
-	}
+    public void ExpandInventory(int additionalSlots)
+    {
+        for (int i = 0; i < additionalSlots; i++)
+        {
+            GameObject newSlot =
+                Resources.Load<GameObject>("Assets/JSFolder/PreFab/Slot.prefab");
+            if (newSlot != null)
+            {
+                Instantiate(newSlot, inventoryContent);
+            }
+        }
 
-	public void ExpandInventory(int additionalSlots) {
-		for (int i = 0; i < additionalSlots; i++) {
-			GameObject newSlot =
-				(GameObject)AssetDatabase.LoadAssetAtPath("Assets/JSFolder/PreFab/Slot.prefab", typeof(GameObject));
+        inventoryData.additionalSlotCount += additionalSlots;
+        SaveInventory();
+        UpdateInventoryUI();
+    }
 
-			if (newSlot != null) {
-				Instantiate(newSlot, inventoryContent);
-			}
-		}
+    public void OnExpandButtonClicked()
+    {
+        ExpandInventory(4);
+    }
 
-		inventoryData.additionalSlotCount += additionalSlots;
-		SaveInventory();
-		UpdateInventoryUI();
-	}
-
-	public void OnExpandButtonClicked() {
-		ExpandInventory(4);
-	}
+    public void Iteminfo()
+    {
+    }
 }
