a4bd2f2ebb204c6ed54e4455a156aced6d57dd84
11lucy22 <winca1122@naver.com>
2024-08-11 15:31:28 +0900

    Fix UnitSlotManager.cs(~ing)

--- Assets/KMS Folder/Script/UnitSlotManager.cs
+++ Assets/KMS Folder/Script/UnitSlotManager.cs
@@ -1,14 +1,14 @@
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.EventSystems;
 using System.Collections.Generic;
 
 public class UnitSlotManager : MonoBehaviour
 {
     public Transform contentParent;  // UnitSlot들을 포함하는 부모 오브젝트
     private List<UnitData> userUnits;  // 유저가 보유한 모든 유닛 데이터 목록
-    private List<Image> unitImages = new List<Image>();  // 각 슬롯의 하위 유닛 이미지 컴포넌트 목록
-
+    private List<Image> unitImages = new List<Image>();  // 각 슬롯의 하위에 있는 유닛 이미지 컴포넌트 목록
+    private List<Graphic> unitSlots = new List<Graphic>(); // 유닛 슬롯(Dropable) 
+    
     private void Awake()
     {
         // 슬롯의 하위 이미지 컴포넌트를 미리 찾아서 저장
@@ -16,10 +16,12 @@ public class UnitSlotManager : MonoBehaviour
         {
             Transform slot = contentParent.GetChild(i);
             Image unitImage = slot.Find("Unit").GetComponent<Image>();
+            Graphic slotGraphic = slot.GetComponent<Graphic>();
 
             if (unitImage != null)
             {
                 unitImages.Add(unitImage);
+                unitSlots.Add(slotGraphic);;
             }
         }
 
@@ -32,7 +34,8 @@ public class UnitSlotManager : MonoBehaviour
     {
         // 슬롯의 개수와 유저가 가진 유닛 개수 중 작은 값을 사용하여 순회
         int unitCount = Mathf.Min(unitImages.Count, userUnits.Count);
-
+        
+        
         // 각 슬롯의 하위 이미지에 유닛 데이터를 할당
         for (int i = 0; i < unitCount; i++)
         {
@@ -42,8 +45,8 @@ public class UnitSlotManager : MonoBehaviour
         // 유저가 가진 유닛보다 슬롯이 많을 경우, 나머지 이미지 비우고 이벤트 비활성화
         for (int i = unitCount; i < unitImages.Count; i++)
         {
-            SetUnitData(unitImages[i], null);  // 이미지 비우기 및 이벤트 비활성화
-            DisableSlotEvents(unitImages[i].transform.parent.gameObject);  // 슬롯의 이벤트 비활성화
+            SetUnitData(unitImages[i], null);
+            unitSlots[i].raycastTarget = false; 
         }
     }
 
@@ -55,48 +58,13 @@ public class UnitSlotManager : MonoBehaviour
             unitImage.color = Color.white;  // 이미지를 기본 색상으로 설정
             unitImage.enabled = true;  // 이미지 표시
 
-            EnableSlotEvents(unitImage.transform.parent.gameObject);  // 슬롯의 이벤트 활성화
+            unitSlots[unitImages.IndexOf(unitImage)].raycastTarget = true;  // 슬롯의 이벤트 활성화
         }
         else
         {
             unitImage.sprite = null;
             unitImage.enabled = false;  // 이미지 숨기기
-            DisableSlotEvents(unitImage.transform.parent.gameObject);  // 슬롯의 이벤트 비활성화
-        }
-    }
-
-    private void EnableSlotEvents(GameObject slot)
-    {
-        // 슬롯의 이벤트를 활성화
-        EventTrigger eventTrigger = slot.GetComponent<EventTrigger>();
-        if (eventTrigger != null)
-        {
-            eventTrigger.enabled = true;
-        }
-    }
-
-    private void DisableSlotEvents(GameObject slot)
-    {
-        // 슬롯의 이벤트를 비활성화
-        EventTrigger eventTrigger = slot.GetComponent<EventTrigger>();
-        if (eventTrigger != null)
-        {
-            eventTrigger.enabled = false;
-        }
-    }
-
-    public UnitData GetUnitData(Image unitImage)
-    {
-        // 해당 이미지에 연결된 UnitData를 반환 (추가 로직 필요 시 구현)
-        return null; // 기본적으로 null 반환, 필요시 구현
-    }
-    
-    // 편성되었을때 유닛창의 유닛 이미지를 어둡게 함
-    public void SetDropped(Image unitImage, bool dropped)
-    {
-        if (unitImage != null)
-        {
-            unitImage.color = dropped ? new Color(0.5f, 0.5f, 0.5f, 1.0f) : Color.white;
+            unitSlots[unitImages.IndexOf(unitImage)].raycastTarget = false;  // 슬롯의 이벤트 비활성화
         }
     }
 }
