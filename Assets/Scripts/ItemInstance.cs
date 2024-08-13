c103e05b312dbf2e169fba2def2781cbe8231d61
mashirokjs <wlsthf9859@gmail.com>
2024-08-13 18:21:10 +0900

    item(id)

---
+++ Assets/Scripts/ItemInstance.cs
@@ -0,0 +1,39 @@
+using System;
+using UnityEngine;
+
+[SerializeField]
+public class ItemInstance
+{
+    public string uniqueID;
+    public string itemID;
+    [NonSerialized] public ItemSO itemData;
+
+    public ItemInstance(ItemSO itemData)
+    {
+        this.itemData = itemData;
+        this.uniqueID = Guid.NewGuid().ToString();
+        this.itemID = itemData.ItemID;
+    }
+
+    public void LoadItemData()
+    {
+        CustomLogger.Log("로드 메서드 실행완료");
+        string[] categories  =
+            { "Armor", "Arrow", "Boots", "Bow", "Gloves", "Helmet", "Shield", "Sprites", "Two-handed", "Wand,Staff" };
+
+        foreach (string category  in categories)
+        {
+            ItemSO[] itemsInCategory = Resources.LoadAll<ItemSO>($"Items/{category}");
+            CustomLogger.Log($"Loading items from category: {category}");
+            foreach (ItemSO item in itemsInCategory)
+            {
+                if (item.ItemID == this.itemID)
+                {
+                    this.itemData = item;
+                    return;
+                }
+            }
+        }
+        CustomLogger.Log($"로드실패! with ID {this.itemID}");
+    }
+}
