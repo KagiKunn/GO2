e7e2a7dc661ec087265e050df7dd8910c78ffb34
hideyoshi515 <hideyoshi515@gmail.com>
2024-08-14 18:08:37 +0900

    Localization & Add 2 BGM

--- Assets/Scripts/DefenseScripts/StageC.cs
+++ Assets/Scripts/DefenseScripts/StageC.cs
@@ -33,7 +33,6 @@ public class StageC : MonoBehaviour {
 
 		// 경로를 출력하여 확인
 		CustomLogger.Log("Save file path: " + saveFilePath);
-CustomLogger
 		// JSON에서 데이터를 불러옴
 		defenseGameData.LoadFromJson(saveFilePath);
 
