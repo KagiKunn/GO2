7c3d269c2fba8132d6872778fbb39daf7100cbee
11lucy22 <winca1122@naver.com>
2024-08-24 20:05:48 +0900

    Fix Drag method in UnitManagement(~ing/Delete method)

--- Assets/KMS Folder/Script/UnitDraggable.cs
+++ Assets/KMS Folder/Script/UnitDraggable.cs
@@ -12,6 +12,7 @@ public class UnitDraggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
     private RectTransform rect;
     private CanvasGroup canvasGroup;
     private GameObject draggingInstance;
+    private RectTransform draggingRect;
 
     private bool isDragging = false;
     private bool isDraggable = true;
@@ -42,38 +43,76 @@ public class UnitDraggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
             isDragging = true;
             previousParent = transform.parent;
             
-            transform.SetParent(Canvas);
-            transform.SetAsLastSibling();
+            Transform unitTransform = previousParent.Find("Unit");
+            unitName = unitTransform.GetComponent<TextMeshProUGUI>().text;
+            GameObject prefabname = unitGameManager.FindPrefabByName(unitName);
             
-            canvasGroup.blocksRaycasts = false;
-            canvasGroup.alpha = 0.6f;
+            draggingInstance = Instantiate(prefabname, Canvas);
+            draggingRect = draggingInstance.GetComponent<RectTransform>();
+
+            CanvasGroup prefabCanvasGroup = draggingInstance.GetComponent<CanvasGroup>();
+            if (prefabCanvasGroup == null)
+            {
+                prefabCanvasGroup = draggingInstance.AddComponent<CanvasGroup>();
+            }
+            
+            var newDraggable = draggingInstance.AddComponent<UnitDraggable>();
+            newDraggable.unitName = unitName;
+            newDraggable.Canvas = Canvas;
+            newDraggable.previousParent = previousParent;
+            newDraggable.mainCamera = mainCamera;
+            newDraggable.unitGameManager = unitGameManager;
+            newDraggable.SetDraggable(true);
+
+            SetDraggingPosition(eventData);
+            
+            draggingRect.sizeDelta = new Vector2(200, 200);
+            draggingRect.localScale = new Vector3(200, 200, 1);
+            
+            rect.gameObject.SetActive(true);
+
+            draggingInstance.transform.SetAsLastSibling();
+            draggingInstance.GetComponent<CanvasGroup>().blocksRaycasts = false;
+            draggingInstance.GetComponent<CanvasGroup>().alpha = 0.6f; 
+            
         }
         
     }
+    
+    private void SetDraggingPosition(PointerEventData eventData)
+    {
+        Vector3 worldPoint;
+        RectTransformUtility.ScreenPointToWorldPointInRectangle(
+            Canvas.GetComponent<RectTransform>(), 
+            eventData.position, 
+            mainCamera, 
+            out worldPoint
+        );
+
+        draggingRect.position = worldPoint;
+    }
+    
     public void OnDrag(PointerEventData eventData)
     {
-        if (isDragging)
-        {
-            Vector2 localPoint;
-            RectTransformUtility.ScreenPointToLocalPointInRectangle(Canvas.GetComponent<RectTransform>(), eventData.position, mainCamera, out localPoint);
-            rect.localPosition = localPoint;
-        }
-
+        SetDraggingPosition(eventData);
     }
     
     public void OnEndDrag(PointerEventData eventData)
     {
-        if (isDragging)
-        {
-            isDragging = false;
+        isDragging = false;
 
-            if (transform.parent == Canvas)
-            {
-                transform.SetParent(previousParent);
-                rect.position = previousParent.GetComponent<RectTransform>().position;
-            }
-            canvasGroup.blocksRaycasts = false;
-            canvasGroup.alpha = 0.6f;
+        Destroy(draggingInstance);
+        draggingInstance = null;
+            
+        if (transform.parent == Canvas)
+        {
+            transform.SetParent(previousParent);
+            rect.position = previousParent.GetComponent<RectTransform>().position;
         }
+        
+        rect.gameObject.SetActive(false);
+        
+        canvasGroup.blocksRaycasts = false;
+        canvasGroup.alpha = 0.6f;
     }
 }
