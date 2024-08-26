cdc66280c86e7d295fc30780591267381b3e4664
KagiKun <key9910@gmail.com>
2024-08-26 17:13:29 +0900

    Feat: next enemy

---
+++ Assets/Scripts/InternalAffairs/NextEnemy.cs
@@ -0,0 +1,16 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+public class NextEnemy : MonoBehaviour
+{
+    public bool isVisual = false;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+        if (isVisual)
+        {
+        }
+    }
+}
