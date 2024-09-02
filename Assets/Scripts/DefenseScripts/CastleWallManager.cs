318ebcbf3fe92a65ea3b474a844da5f3a4ecae05
PJW <PJW>
2024-09-02 16:32:08 +0900

    show destroy castle when game is over

--- Assets/Scripts/DefenseScripts/CastleWallManager.cs
+++ Assets/Scripts/DefenseScripts/CastleWallManager.cs
@@ -7,7 +7,8 @@ using UnityEngine.Audio;
 
 #pragma warning disable CS0618 // 형식 또는 멤버는 사용되지 않습니다.
 
-public class CastleWallManager : MonoBehaviour {
+public class CastleWallManager : MonoBehaviour
+{
     public static CastleWallManager Instance;
 
     [SerializeField] private float activateShieldValue = 80f; // 기본 실드 최대값
@@ -33,14 +34,20 @@ public class CastleWallManager : MonoBehaviour {
     public AudioClip soundClip;
     private AudioSource audioSource;
     public AudioMixerGroup sfxMixerGroup;
-    private void Awake() {
+
+    private GameObject[] destroyedWalls;
+
+    private void Awake()
+    {
         Debug.Log("캐슬월에서 가져온 스테이지카운트 : " + PlayerLocalManager.Instance.lStage);
-        if (PlayerLocalManager.Instance.lStage == 1) {
-            
+        if (PlayerLocalManager.Instance.lStage == 1)
+        {
             health = maxHealth;
         }
+
         // Load game data
-        if (PlayerLocalManager.Instance != null) {
+        if (PlayerLocalManager.Instance != null)
+        {
             maxHealth = PlayerLocalManager.Instance.lCastleMaxHp;
             health = PlayerLocalManager.Instance.lCastleHp;
         }
@@ -49,9 +56,12 @@ public class CastleWallManager : MonoBehaviour {
         wallObjects.Add(GameObject.FindGameObjectsWithTag("RightWall")[0]);
         wallObjects.Add(GameObject.FindGameObjectsWithTag("LeftWall")[0]);
 
-        if (Instance == null) {
+        if (Instance == null)
+        {
             Instance = this;
-        } else {
+        }
+        else
+        {
             Destroy(gameObject);
         }
 
@@ -61,26 +71,41 @@ public class CastleWallManager : MonoBehaviour {
         InitializeSlider();
 
         stageC = FindObjectOfType<StageC>();
+
+        // DestroyedWalls를 클래스 멤버로 초기화하고 비활성화
+        destroyedWalls = GameObject.FindGameObjectsWithTag("DestroyedWalls");
+        foreach (GameObject wall in destroyedWalls)
+        {
+            wall.SetActive(false); // 오브젝트를 비활성화하여 보이지 않게 합니다.
+        }
     }
 
-    private void InitializeSlider() {
-        if (healthSlider != null) {
+    private void InitializeSlider()
+    {
+        if (healthSlider != null)
+        {
             healthSlider.maxValue = maxHealth;
             healthSlider.value = health;
             fillImage.color = Color.green; // 기본 색상
             handleImage.sprite = healthIcon; // 기본 체력 아이콘 설정
         }
     }
+
     private void Start()
     {
         audioSource = gameObject.AddComponent<AudioSource>();
         audioSource.clip = soundClip;
         audioSource.outputAudioMixerGroup = sfxMixerGroup;
     }
-    private void Update() {
-        if (!hasShield) {
-            foreach (var wall in wallObjects) {
-                if (wall != null) {
+
+    private void Update()
+    {
+        if (!hasShield)
+        {
+            foreach (var wall in wallObjects)
+            {
+                if (wall != null)
+                {
                     CastleWall cwall = wall.GetComponent<CastleWall>();
                     cwall.ChangeWallColor(false); // 실드 비활성화 시 색상 변경
                 }
@@ -88,11 +113,13 @@ public class CastleWallManager : MonoBehaviour {
         }
 
         // hasShield가 false가 되었을 때 실드 비활성화
-        if (!hasShield && shield > 0) {
+        if (!hasShield && shield > 0)
+        {
             SetShield(0); // 실드를 비활성화하고 초기화
             Debug.Log("Shield deactivated.");
         }
     }
+
     public void PlaySound()
     {
         if (audioSource != null && soundClip != null)
@@ -100,24 +127,31 @@ public class CastleWallManager : MonoBehaviour {
             audioSource.Play();
         }
     }
-    public void ApplyDamage(float damage) {
-        if (hasShield) {
+
+    public void ApplyDamage(float damage)
+    {
+        if (hasShield)
+        {
             shield -= damage;
 
-            if (shield <= 0) {
+            if (shield <= 0)
+            {
                 damage = -shield; // 남은 데미지를 체력에 적용
                 shield = 0;
                 hasShield = false;
 
                 Debug.Log("Shield destroyed!");
-            } else {
+            }
+            else
+            {
                 damage = 0;
             }
         }
 
         health -= damage;
 
-        if (health <= 0) {
+        if (health <= 0)
+        {
             health = 0;
             HandleGameOver();
         }
@@ -125,7 +159,8 @@ public class CastleWallManager : MonoBehaviour {
         UpdateSliders();
     }
 
-    public void EarnShield(float duration, float shieldAmount) {
+    public void EarnShield(float duration, float shieldAmount)
+    {
         Debug.Log("EarnShield called with duration: " + duration + " and shieldAmount: " + shieldAmount);
 
         // 슬라이더의 최대값을 전달받은 shieldAmount로 설정
@@ -133,11 +168,13 @@ public class CastleWallManager : MonoBehaviour {
 
         hasShield = true;
 
-        if (resetShieldCoroutine != null) {
+        if (resetShieldCoroutine != null)
+        {
             StopCoroutine(resetShieldCoroutine);
         }
 
-        foreach (var wall in wallObjects) {
+        foreach (var wall in wallObjects)
+        {
             CastleWall cwall = wall.GetComponent<CastleWall>();
             cwall.ChangeWallColor(true);
         }
@@ -149,14 +186,17 @@ public class CastleWallManager : MonoBehaviour {
         Debug.Log("Shield activated with shield amount: " + shield);
     }
 
-    private IEnumerator ResetEarnShieldAfterDelay(float delay) {
-        if (!hasShield) {
+    private IEnumerator ResetEarnShieldAfterDelay(float delay)
+    {
+        if (!hasShield)
+        {
             yield break;
         }
 
         yield return new WaitForSeconds(delay);
 
-        foreach (var wall in wallObjects) {
+        foreach (var wall in wallObjects)
+        {
             CastleWall cwall = wall.GetComponent<CastleWall>();
             cwall.ChangeWallColor(false);
         }
@@ -166,29 +206,36 @@ public class CastleWallManager : MonoBehaviour {
         Debug.Log("Shield reset to original values.");
     }
 
-    public void AddShield(float shieldAmount) {
+    public void AddShield(float shieldAmount)
+    {
         shield = Mathf.Min(activateShieldValue, shield + shieldAmount);
         hasShield = shield > 0;
         UpdateSliders();
         Debug.Log("Shield added. Current shield: " + shield);
     }
 
-    public void SetShield(float shieldValue) {
+    public void SetShield(float shieldValue)
+    {
         shield = Mathf.Clamp(shieldValue, 0, activateShieldValue);
         hasShield = shield > 0;
         UpdateSliders();
         Debug.Log("Shield set to value: " + shield);
     }
 
-    private void UpdateSliders() {
-        if (healthSlider != null) {
-            if (hasShield) {
+    private void UpdateSliders()
+    {
+        if (healthSlider != null)
+        {
+            if (hasShield)
+            {
                 // 실드가 활성화된 경우: 슬라이더를 실드 값으로 설정
                 healthSlider.maxValue = activateShieldValue;
                 healthSlider.value = shield;
                 fillImage.color = Color.blue; // 실드 색상
                 handleImage.sprite = shieldIcon; // 실드 아이콘
-            } else {
+            }
+            else
+            {
                 // 실드가 비활성화된 경우: 슬라이더를 체력 값으로 설정
                 healthSlider.maxValue = maxHealth;
                 healthSlider.value = health;
@@ -203,32 +250,46 @@ public class CastleWallManager : MonoBehaviour {
         PlaySound();
         DestroyWallsWithTag("TagForDestroyWalls");
 
+        ShowDestroyedWalls();
         Debug.Log("성벽이 파괴되었습니다! 게임 오버!");
 
-        if (stageC != null) {
+        if (stageC != null)
+        {
             stageC.ShowGameOverUI();
-        } else {
+        }
+        else
+        {
             Debug.LogWarning("StageC instance not found.");
         }
 
         HeroManager.Instance.ClearHeroFormation();
     }
 
-    private void DestroyWallsWithTag(string tag) {
+    private void DestroyWallsWithTag(string tag)
+    {
         GameObject[] walls = GameObject.FindGameObjectsWithTag(tag);
 
-        foreach (GameObject wall in walls) {
+        foreach (GameObject wall in walls)
+        {
             Destroy(wall);
         }
+        
+    }
 
-        GameObject[] destroyedWalls = GameObject.FindGameObjectsWithTag("DestroyedWalls");
-        foreach (GameObject wall in destroyedWalls) {
-            wall.SetActive(true);
+    private void ShowDestroyedWalls()
+    {
+        foreach (GameObject wall in destroyedWalls)
+        {
+            if (wall != null)
+            {
+                wall.SetActive(true); // 오브젝트를 활성화하여 보이게 합니다.
+            }
         }
     }
     
 
-    public void SaveWallHP() {
+    public void SaveWallHP()
+    {
         CustomLogger.Log("성벽 HP 정보 저장됨 health, mH, eH" + health + "," + maxHealth);
         PlayerLocalManager.Instance.lCastleHp = health;
         PlayerLocalManager.Instance.lCastleMaxHp = maxHealth;
